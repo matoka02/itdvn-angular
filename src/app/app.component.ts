dce152927c6967c2fd1bffa4708b64caf9e18b5e
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-01-14 15:25:23 +0200

    update lesson-04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // standalone: true,
-  // imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'itvdn-angular';
+
+  // директивы
+
+  // isAdmin: boolean = true;
+  // changeAdminStatus(){
+  //   this.isAdmin = !this.isAdmin;
+  // };
+
+  // users = [
+  //   { userName: 'Andrii', age: 21 },
+  //   { userName: 'Anna', age: 25 },
+  //   { userName: 'Max', age: 19 },
+  // ];
+
+  // car = 'Audi';
+
+  // elementStyle = {
+  //   color: '#BE3455',
+  //   opacity: 0.5
+  // };
+  // isAdmin: boolean = false;
+  // arrayOfClasses = ['red', 'bold'];
+
+
+  // пайпы
+  currentDate = new Date();
+  users = [
+    { userName: 'Andrii', age: 21 },
+    { userName: 'Anna', age: 25 },
+    { userName: 'Max', age: 19 },
+  ];
 }
