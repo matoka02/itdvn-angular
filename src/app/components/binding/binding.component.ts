465b4a0fe3eeb6a29eb141002ee61b2f07d4f34e
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-01-16 22:38:09 +0200

    update lesson-09

--- src/app/components/binding/binding.component.ts
+++ src/app/components/binding/binding.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-binding',
@@ -16,19 +16,50 @@ export class BindingComponent {
   // формы
   loginForm: FormGroup;
 
+  // regexp = /\+38\(0[0-9]{2}\)-[0-9]{3}-\d{2}-\d{2}/
+  regexp = '';
+
+  isAuthorized = false;
+
   constructor() {
     this.loginForm = new FormGroup({
       email: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4)]),
-      agree: new FormControl(false, Validators.requiredTrue),
-      name: new FormGroup({
-        firstName: new FormControl(''),
-        lastName: new FormControl(''),
-      }),
+      // agree: new FormControl(false, Validators.requiredTrue),
+      // name: new FormGroup({
+      //   firstName: new FormControl(''),
+      //   lastName: new FormControl(''),
+      // }),
+      contacts: new FormArray([new FormControl('+38(0', [Validators.pattern(this.regexp), Validators.required])]),
     })
   };
 
-  onSubmit(){
-    this.loginForm.reset()
+  // onSubmit() {
+  //   this.loginForm.reset()
+  // };
+
+  onSubmit() {
+    this.isAuthorized = true;
+    this.loginForm.reset();
+    this.createNewControl();
   };
+
+  createNewControl(){
+    this.loginForm.addControl('subscribe', new FormControl(false));
+    this.loginForm.get('subscribe')?.setValidators(Validators.required);
+  };
+
+  get getContacts(): FormArray {
+    return this.loginForm.get('contacts') as FormArray
+  };
+
+  addPhone() {
+    this.getContacts.push(new FormControl('+38(0', [Validators.pattern(this.regexp), Validators.required]))
+  };
+
+  clearPhones() {
+    this.getContacts.controls.splice(1);
+  };
+
+
 }
