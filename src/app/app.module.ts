fe3c6a3622c0f064311b075b13c68a007d870af3
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-01-16 19:35:03 +0200

    update AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing-module';
 
 @NgModule({
-  declarations:[AppComponent],
-  imports: [BrowserModule],
-  providers:[],
-  bootstrap:[AppComponent]
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule{}
+export class AppModule { }
